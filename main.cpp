3c53b142631ffed2ae4cfb48f0c8a7e3ff251a73
tymi <tymonszyler@gmail.com>
2025-08-31 12:17:21 +0200

    pointers

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "pointers.h"
+
+int main() {
+    pointers();
+    return 0;
+}
---
+++ pointers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void pointers(){
+
+    int x = 5;
+    int* pointer = &x;
+    cout << pointer <<endl;
+
+
+}
