567f9131c312176988a603f4c165b6cca9e29b6c
tymi <tymonszyler@gmail.com>
2025-09-03 21:05:40 +0200

    BST TREE AND I REMEMBER EVERYTHINGGGG

--- algorithms.cpp
+++ algorithms.cpp
@@ -4,6 +4,55 @@
 #include <set>
 using namespace std;
 
+
+struct BST{
+    BST* lewo;
+    BST* prawo;
+    int wartosc;
+};
+BST* nowy_wezel(BST*& korzen,int w){
+
+
+    if(korzen == nullptr){
+        BST* tmp = new BST;
+        tmp->wartosc = w;
+        tmp->lewo = nullptr;
+        tmp->prawo = nullptr;
+        korzen= tmp;
+        return korzen;
+    }
+    if( korzen->wartosc> w){
+        nowy_wezel(korzen->lewo,w);
+    }
+    if(korzen->wartosc <= w){
+        nowy_wezel(korzen->prawo,w);
+    }
+    return korzen;
+}
+BST* finding_value(BST*& korzen,int value){
+
+    if(korzen == nullptr){
+        cout<<"\nnie ma tej liczby w tym drzewie BST\n";
+        return nullptr;
+
+    }
+    if(korzen->wartosc == value) {
+        cout << "\nznaleziono liczbe " << korzen->wartosc << " w tym drzewie BST!\n";
+        return korzen;
+
+    }
+    if(korzen->wartosc> value){
+        finding_value(korzen->lewo,value);
+    }
+    if(korzen->wartosc<=value){
+        finding_value(korzen->prawo,value);
+    }
+    return korzen;
+
+
+
+}
+
 int liczenie_unikalnych_znakow(string string1) {
     cout << "\nSTRING:   " << string1;
 
@@ -76,8 +125,20 @@ void algorithms(){
 //    liczenie_unikalnych_znakow("aaaaa bbbb- cccc! -- d");
 //    set_unikalne_znaki("aaaaa bbbb- cccc! -- d");
 //    cout << "\n\n"<<is_pallindrom(121);
-    vector<int> z={1,3,5,7,11,13,17,19,23};
-    cout << binary_search(z,17);
+//    vector<int> z={1,3,5,7,11,13,17,19,23};
+//    cout << binary_search(z,17);
+    BST* korzen = nullptr;
+    nowy_wezel(korzen,5);
+    nowy_wezel(korzen,1);
+    nowy_wezel(korzen,15);
+    nowy_wezel(korzen,12);
+    nowy_wezel(korzen,7);
+    nowy_wezel(korzen,100);
+    finding_value(korzen,13);
+
+
+
+
 }
 
 
